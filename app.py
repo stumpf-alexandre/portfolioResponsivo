a71e1a90dae01ef50a7998dc7315ac9d2357d2c8
stumpf-alexandre <als.stumpf@gmail.com>
2022-02-11 02:10:32 -0300

    criando barra de navegação lateral  e estilizando links, icones e imagens

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from socket import if_nameindex
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
@@ -8,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
